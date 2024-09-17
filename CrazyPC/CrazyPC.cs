9278ae9a45698754b79a2afbaf92b63f3ed4faf4
Senior <Senior@G-02-06>
2024-09-17 13:27:10 -0400

    Add project files.

---
+++ CrazyPC/CrazyPC.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+using System.Media;
+
+namespace CrazyPC
+{
+    internal class CrazyPC
+    {
+        public static Random _random = new Random();
+        private static CancellationTokenSource _cts;
+
+        static void CrazyMouseThread(CancellationToken token)
+        {
+            Console.WriteLine("CrazyMouseThread");
+
+            while (!token.IsCancellationRequested)
+            {
+                int moveX = _random.Next(-10, 10);
+                int moveY = _random.Next(-10, 10);
+
+                Cursor.Position = new Point(Cursor.Position.X + moveX, Cursor.Position.Y + moveY);
+                Thread.Sleep(50);
+            }
+        }
+
+        static void CrazyKeyboardThread(CancellationToken token)
+        {
+            Console.WriteLine("CrazyKeyboardThread");
+
+            while (!token.IsCancellationRequested)
+            {
+                if (_random.Next(0, 100) < 50)
+                {
+                    SendKeys.SendWait("a");
+                }
+                else
+                {
+                    SendKeys.SendWait("b");
+                }
+
+                Thread.Sleep(50);
+            }
+        }
+
+        static void CrazySoundThread(CancellationToken token)
+        {
+            Console.WriteLine("CrazySoundThread");
+
+            while (!token.IsCancellationRequested)
+            {
+                if (_random.Next(0, 100) < 50)
+                {
+                    SystemSounds.Beep.Play();
+                }
+                else
+                {
+                    SystemSounds.Exclamation.Play();
+                }
+
+                Thread.Sleep(50);
+            }
+        }
+
+        public static void CrazyFunctionCall()
+        {
+            _cts = new CancellationTokenSource();
+            CancellationToken token = _cts.Token;
+
+            Thread crazyMouseThread = new Thread(() => CrazyMouseThread(token));
+            Thread crazyKeyboardThread = new Thread(() => CrazyKeyboardThread(token));
+            Thread crazySoundThread = new Thread(() => CrazySoundThread(token));
+
+            crazyMouseThread.Start();
+            crazyKeyboardThread.Start();
+            crazySoundThread.Start();
+            DateTime future = DateTime.Now.AddSeconds(10);
+            while (future > DateTime.Now)
+            {
+                Thread.Sleep(1000);
+            }
+
+            _cts.Cancel();
+
+            // Wait for threads to finish gracefully
+            crazyMouseThread.Join();
+            crazyKeyboardThread.Join();
+            crazySoundThread.Join();
+        }
+    }
+}
---
+++ CrazyPC/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrazyPC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CrazyPC.CrazyFunctionCall();
+
+            Console.ReadKey();
+        }
+    }
+}
